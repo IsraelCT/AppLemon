d562c4aa58c6987186df6471dd033beccac82ca5
Tornado <isracobtor@gmail.com>
2025-04-15 23:39:43 -0600

    Colocación del texto e imagen con el uso de When

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.applemon"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.applemon"
--- app/src/main/java/com/example/applemon/MainActivity.kt
+++ app/src/main/java/com/example/applemon/MainActivity.kt
@@ -4,13 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.applemon.ui.theme.AppLemonTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +35,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AppLemonTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    AppLemon(
                     )
                 }
             }
@@ -30,18 +43,75 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
+
 @Composable
-fun GreetingPreview() {
-    AppLemonTheme {
-        Greeting("Android")
+fun AppLemon() {
+    var currentStep by remember { mutableStateOf(1) }
+    var imageResource = when (currentStep) {
+        1 -> Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = stringResource(R.string.Tap_the_lemon_tree))
+            Spacer(modifier = Modifier.height(32.dp))
+            Image(
+                painter = painterResource(R.drawable.lemon_tree),
+                contentDescription = stringResource(R.string.Lemon),
+                modifier = Modifier
+                    .wrapContentSize()
+                    .clickable { currentStep = 2 })
+        }
+
+        2 -> Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = stringResource(R.string.Keep_tapping))
+            Spacer(modifier = Modifier.height(32.dp))
+            Image(
+                painter = painterResource(R.drawable.lemon_squeeze),
+                contentDescription = null,
+                modifier = Modifier
+                    .wrapContentSize()
+                    .clickable { currentStep = 3 })
+
+        }
+
+        3 -> Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = stringResource(R.string.Tap_the_lemonade_to_drink))
+            Spacer(modifier = Modifier.height(32.dp))
+            Image(
+                painter = painterResource(R.drawable.lemon_drink),
+                contentDescription = null,
+                modifier = Modifier
+                    .wrapContentSize()
+                    .clickable { currentStep = 4 })
+
+        }
+
+        else -> Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = stringResource(R.string.Tap_the_empty))
+            Spacer(modifier = Modifier.height(32.dp))
+            Image(
+                painter = painterResource(R.drawable.lemon_restart),
+                contentDescription = null,
+                modifier = Modifier
+                    .wrapContentSize()
+                    .clickable { currentStep = 1 })
+
+
+        }
     }
 }
+
